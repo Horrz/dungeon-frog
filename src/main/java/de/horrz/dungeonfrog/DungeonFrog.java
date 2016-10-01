68d5ed819b692b9d9a509696e63b758785a951cb
horrz <martin.horatschek@googlemail.com>
2016-10-01 20:34:57 +0200

    added basic orthogonal projections with 2 boxes drawn on the screen

--- src/main/java/de/horrz/dungeonfrog/DungeonFrog.java
+++ src/main/java/de/horrz/dungeonfrog/DungeonFrog.java
@@ -11,12 +11,14 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 //Containing the Main Window and Loop
 public class DungeonFrog {
+    private final static int WIDTH = 600;
+    private final static int HEIGHT = 600;
 
     // The window handle
     private long window;
     private boolean running = true;
 
-    public void run() {
+    private void run() {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
         try {
@@ -47,9 +49,6 @@ public class DungeonFrog {
         glfwDefaultWindowHints(); // optional, the current window hints are already the default
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-        int WIDTH = 300;
-        int HEIGHT = 300;
 
         // Create the window
         window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
@@ -103,8 +102,44 @@ public class DungeonFrog {
     }
 
     private void render() {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-        glfwSwapBuffers(window); // swap the color buffers
+        // clear the framebuffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        renderHUD();
+
+        // swap the color buffers
+        glfwSwapBuffers(window);
+    }
+
+    private void renderHUD() {
+        glDisable(GL_DEPTH_TEST);
+        glDepthMask(false);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, WIDTH, 0, HEIGHT, -1, 1);
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        glColor3f(0.0f,0.0f,1.0f);
+        // drawing the HUD rect here:
+        glBegin(GL_QUADS);
+            glVertex2f(50,50);
+            glVertex2f(50,100);
+            glVertex2f(100,100);
+            glVertex2f(100,50);
+        glEnd();
+
+        glBegin(GL_QUADS);
+            glVertex2f(150,150);
+            glVertex2f(150,200);
+            glVertex2f(200,200);
+            glVertex2f(200,150);
+        glEnd();
+
+        glDepthMask(true);
+        glPopMatrix();
     }
 
     private void update() {
